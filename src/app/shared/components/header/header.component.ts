2dc49ae729f602afee474f03c172a1acff113d66
Lais <laisffn@hotmail.com>
2021-10-05 22:39:17 -0300

    exporta dados para excel

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
 // import { AuthenticationService } from '../../authentication/authentication.service';
 
 @Component({
@@ -11,14 +12,27 @@ export class HeaderComponent {
   @Input() title: string = '';
   @Input() subtitle: string = '';
   @Input() showButton: boolean = false;
+  @Input() idTable: string = '';
+
+  fileName = 'ExcelSheet.xlsx';
 
   constructor(
     // public authenticationService: AuthenticationService
     private router: Router
-  ) { }
+  ) {}
 
   logout(): void {
     // this.authenticationService.SignOut();
-    this.router.navigate(['/'])
+    this.router.navigate(['/']);
+  }
+
+  export(idTable: string): void {
+    let element = document.getElementById(idTable);
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    XLSX.writeFile(wb, this.fileName);
   }
 }
